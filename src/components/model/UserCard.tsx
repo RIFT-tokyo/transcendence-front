e8c12a65986a9851aa30e9f7a945f839ba98efcc
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-01-22 19:58:36 +0900

    feat: modify layout

--- src/components/model/UserCard.tsx
+++ src/components/model/UserCard.tsx
@@ -3,7 +3,11 @@ import { ResponseUser } from '../../api/generated/api';
 
 const UserCard: React.VFC<{user: ResponseUser | null}> = ({user}) => {
   return (
-    <Card sx={{ width: 320, height: 384 }}>
+    <Card sx={{
+      width: 320,
+      height: 384,
+      margin: '20px',
+    }}>
       <CardContent>
         <Avatar sx={{ width: 256, height: 256 }} />
         <Typography sx={{fontWeight: "bold"}} variant="h4">{user?.name}</Typography>
