ca53eb1a7b438de1e1e6156b991385cf38cbaa04
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-01-25 21:28:03 +0900

    feat: create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import theme from './components/config/theme';
 import UserProfile from './components/pages/UserProfile';
 import AppBarWithMenu from './components/ui/AppBarWithMenu';
@@ -7,8 +8,18 @@ const App = () => {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <AppBarWithMenu />
-        <UserProfile />
+        <Routes>
+          <Route path="/" element={<AppBarWithMenu/>} >
+            <Route index element={<Navigate to="/home"/>} />
+            <Route path="home" element={<UserProfile/>} />
+            <Route path="chat" element={<div>Chat</div>} />
+            <Route path="pong" element={<div>Pong</div>} />
+            <Route path="settings" element={<div>Settings</div>} />
+            <Route path="404" element={<div>404 Not found</div>} />
+            <Route path="500" element={<div>500 Internal server error</div>} />
+            <Route path="*" element={<Navigate to="/404"/>} />
+          </Route>
+        </Routes>
       </ThemeProvider>
     </div>
   );
