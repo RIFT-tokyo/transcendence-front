74785327590d5f493143f8074fcec85bb63aee1b
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-02-12 20:19:39 +0900

    feat: justify center

--- src/components/pages/InternalServerError.tsx
+++ src/components/pages/InternalServerError.tsx
@@ -1,13 +1,13 @@
-import { Container, Stack, Typography } from '@mui/material'
+import { Grid, Stack, Typography } from '@mui/material'
 
 const InternalServerError = () => {
   return (
-    <Container>
+    <Grid container justifyContent='center'>
       <Stack spacing={1} margin={20}>
         <img src="/error/500.svg" alt="500" />
         <Typography padding={5} align='center' variant="h3">Internal Server Error</Typography>
       </Stack>
-    </Container>
+    </Grid>
   )
 }
 
--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,13 +1,13 @@
-import { Container, Stack, Typography } from '@mui/material';
+import { Grid, Stack, Typography } from '@mui/material';
 
 const NotFound = () => {
   return (
-    <Container>
+    <Grid container justifyContent='center'>
       <Stack spacing={1} margin={20}>
         <img src="/error/404.svg" alt="404" />
         <Typography padding={5} align='center' variant="h3">Not Found</Typography>
       </Stack>
-    </Container>
+    </Grid>
   )
 }
 
