0a9fce6bf7bfe6930573b4abe211f9a1f762d6a9
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-02-21 22:53:58 +0900

    feat: set each icon

--- src/components/ui/SettingTab.tsx
+++ src/components/ui/SettingTab.tsx
@@ -1,5 +1,7 @@
 import { Link, Stack, Typography } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person';
+import KeyIcon from '@mui/icons-material/Key';
+import HelpIcon from '@mui/icons-material/Help';
 import { NavLink } from 'react-router-dom';
 
 type Props = {
@@ -7,12 +9,22 @@ type Props = {
 }
 
 const SettingTab: React.VFC<Props> = ({ actions }) => {
+  const getIcon = (action: string) => {
+    switch (action) {
+      case 'Account':
+        return <PersonIcon />;
+      case 'Security':
+        return <KeyIcon />;
+      default:
+        return <HelpIcon />;
+    }
+  }
   let settings = actions.map(item => {
     const link = '/settings/' + item.toLowerCase();
     return (
       <Link key={item.toLowerCase()} component={NavLink} color='inherit' underline='none' to={link}>
-        <Stack direction='row' spacing={2}>
-          <PersonIcon />
+        <Stack direction='row' spacing={2} alignItems='center'>
+          {getIcon(item)}
           <Typography variant='h5'>{item}</Typography>
         </Stack>
       </Link>
