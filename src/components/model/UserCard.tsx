36a39dd5a49a4451e123c1e8a02d827e9a35e2fe
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-02-23 14:59:20 +0900

    feat: add link to setting

--- src/components/model/UserCard.tsx
+++ src/components/model/UserCard.tsx
@@ -1,7 +1,22 @@
-import { Avatar, Button, Card, CardContent, Typography } from '@mui/material'
+import { Avatar, Button, Card, CardContent, Typography, Link } from '@mui/material'
+import { NavLink } from 'react-router-dom';
 import { ResponseUser } from '../../api/generated/api';
 
 const UserCard: React.VFC<{user: ResponseUser | null, isOwner: boolean}> = ({user, isOwner}) => {
+  const ActionButton = (isOwner: boolean) => {
+    if (isOwner) {
+      return (
+        <Link component={NavLink} color='inherit' underline='none' to='/settings'>
+          <Button sx={{ width: 296, height: 30 }} color='inherit' variant='contained'>Edit Profile</Button>
+        </Link>
+      )
+    } else {
+      return (
+        <Button sx={{ width: 296, height: 30 }} color='inherit' variant='contained'>{isOwner ? "Edit Profile" : "Follow"}</Button>
+      )
+    }
+  }
+
   return (
     <Card
       sx={{
@@ -12,7 +27,7 @@ const UserCard: React.VFC<{user: ResponseUser | null, isOwner: boolean}> = ({use
         <Avatar sx={{ width: 296, height: 296 }} src={user?.profile_image}/>
         <Typography sx={{fontWeight: "bold"}} variant="h4">{user?.display_name ?? user?.username}</Typography>
         <Typography variant="subtitle1">{user?.username}</Typography>
-        <Button sx={{ width: 296, height: 30 }} color='inherit' variant='contained'>{isOwner ? "Edit Profile" : "Follow"}</Button>
+        {ActionButton(isOwner)}
         <Typography variant="body2">{user?.status_message}</Typography>
       </CardContent>
     </Card>
