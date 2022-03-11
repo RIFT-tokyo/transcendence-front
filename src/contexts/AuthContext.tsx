6adfb4936c5d47aab58f0128599e51c3a32bdb71
sasakiyudai <yudai14142@gmail.com>
2022-03-11 21:31:11 +0900

    AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,26 +1,50 @@
-import React, { FC, useState } from 'react';
+import Cookies from 'js-cookie';
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import { User, UserApi } from '../api/generated/api';
 
-export const AuthContext = React.createContext<any>(null);
+interface IAuthContext {
+  currentUser: User | null;
+  login: () => void;
+  logout: () => void;
+}
+
+const defaultState = {
+  currentUser: null,
+  login: () => {},
+  logout: () => {},
+};
+
+export const AuthContext = React.createContext<IAuthContext>(defaultState);
 
 export const AuthProvider: FC = ({ children }) => {
-  // User is the name of the "data" that gets stored in context
-  const [user, setUser] = useState({ name: '', auth: true });
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const userApi = new UserApi();
 
-  // Login updates the user data with a name parameter
-  const login = (name: string) => {
-    setUser((user) => ({
-      name,
-      auth: true,
-    }));
+  const fetchMe = async () => {
+    const owner = await userApi
+      .getMe({ withCredentials: true })
+      .then((res) => res.data)
+      .catch(() => null);
+    return owner;
   };
 
-  // Logout updates the user data to default
+  const login = async () => {
+    const owner = await fetchMe();
+    setCurrentUser(owner);
+  };
+
   const logout = () => {
-    setUser((user) => ({
-      name: '',
-      auth: false,
-    }));
+    setCurrentUser(null);
   };
 
-  return <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>;
+  useEffect(() => {
+    (async () => {
+      await login();
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const memo = useMemo(() => ({ currentUser, login, logout }), [currentUser]);
+
+  return <AuthContext.Provider value={memo}>{children}</AuthContext.Provider>;
 };
