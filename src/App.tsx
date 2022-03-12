cb5534abdc97a3379b97754a49bb1ef6db26339e
sasakiyudai <yudai14142@gmail.com>
2022-03-12 23:43:10 +0900

    private route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from '@mui/material';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
 import theme from './components/config/theme';
 import Chat from './components/pages/Chat';
 import InternalServerError from './components/pages/InternalServerError';
@@ -10,16 +11,20 @@ import SignUp from './components/pages/SignUp';
 import Settings from './components/pages/Settings';
 import UserProfile from './components/pages/UserProfile';
 import AppBarWithMenu from './components/ui/AppBarWithMenu';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthContext } from './contexts/AuthContext';
+
+const PrivateRoute = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  return currentUser ? <Outlet /> : <Navigate to="signin" />;
+};
 
 const App = () => (
   <div className="App">
-    <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <Routes>
-          <Route path="/" element={<AppBarWithMenu />}>
-            <Route index element={<SignUp />} />
-            <Route path="signin" element={<SignIn />} />
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<AppBarWithMenu />}>
+          <Route path="" element={<PrivateRoute />}>
             <Route path="home" element={<UserProfile />} />
             <Route path="users">
               <Route path=":username" element={<UserProfile />} />
@@ -31,13 +36,15 @@ const App = () => (
               <Route path="account" element={<Settings active="Account" />} />
               <Route path="security" element={<Settings active="Security" />} />
             </Route>
-            <Route path="404" element={<NotFound />} />
-            <Route path="500" element={<InternalServerError />} />
-            <Route path="*" element={<Navigate to="404" />} />
           </Route>
-        </Routes>
-      </ThemeProvider>
-    </AuthProvider>
+          <Route index element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="404" element={<NotFound />} />
+          <Route path="500" element={<InternalServerError />} />
+          <Route path="*" element={<Navigate to="404" />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   </div>
 );
 
