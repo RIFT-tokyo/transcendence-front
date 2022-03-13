573b2193d2bb165798d12318a285e27f8437cc51
sasakiyudai <yudai14142@gmail.com>
2022-03-13 14:13:28 +0900

    setCurrentUser null when logout

--- src/components/ui/AppBarWithMenu.tsx
+++ src/components/ui/AppBarWithMenu.tsx
@@ -1,13 +1,13 @@
 import { AppBar, Toolbar, IconButton, Typography, Box, Menu, MenuItem, Link } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { AccountCircle } from '@mui/icons-material';
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { AuthApi } from '../../api/generated/api';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const AppBarWithMenu = () => {
-  const [auth] = useState(true);
+  const { logout } = useContext(AuthContext);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const navigate = useNavigate();
   const authApi = new AuthApi();
@@ -30,11 +30,12 @@ const AppBarWithMenu = () => {
     navigate('/settings');
   };
 
-  const logout = async () => {
+  const logOut = async () => {
     handleClose();
     await authApi
       .postAuthLogout({ withCredentials: true })
       .then(() => {
+        logout();
         goHome();
       })
       .catch((err) => {
@@ -53,39 +54,37 @@ const AppBarWithMenu = () => {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               ft_transcendence
             </Typography>
-            {auth && (
-              <div>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  <MenuItem onClick={goHome}>Profile</MenuItem>
-                  <MenuItem onClick={goToSettings}>Settings</MenuItem>
-                  <MenuItem onClick={logout}>Logout</MenuItem>
-                </Menu>
-              </div>
-            )}
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={goHome}>Profile</MenuItem>
+                <MenuItem onClick={goToSettings}>Settings</MenuItem>
+                <MenuItem onClick={logOut}>Logout</MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
       </Box>
