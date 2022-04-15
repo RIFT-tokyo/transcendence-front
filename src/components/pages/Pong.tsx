6bf3f357ab2741978bd779487a992c03afb35ca7
sasakiyudai <yudai14142@gmail.com>
2022-04-15 15:42:02 +0900

    emit ping and userStatus on Pong.tsx

--- src/components/pages/Pong.tsx
+++ src/components/pages/Pong.tsx
@@ -1,5 +1,21 @@
 import { Typography } from '@mui/material';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { SocketContext } from '../../contexts/SocketContext';
 
-const Pong = () => <Typography variant="h2">This page is for Pong</Typography>;
+const Pong = () => {
+  const { client } = useContext(SocketContext);
+  const { authUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    client.emit('ping');
+	client.emit('userStatus', { status: 'game', userID: authUser?.id });
+    return () => {
+      client.emit('userStatus', { status: 'online', userID: authUser?.id });
+    };
+  }, []);
+
+  return <Typography variant="h2">This page is for Pong</Typography>;
+};
 
 export default Pong;
