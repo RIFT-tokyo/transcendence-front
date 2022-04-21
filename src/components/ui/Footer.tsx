1550e9e29dca319b499d7eba3d6be3648b4125e1
sasakiyudai <yudai14142@gmail.com>
2022-04-21 14:20:59 +0900

    make GlobalFooter components

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,22 +1,55 @@
-import { BottomNavigation, Box, Grid, Paper } from '@mui/material';
 import * as React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
-type Props = {
-  children: React.ReactNode;
-};
+const Copyright = () => (
+  <Typography variant="body2" color="text.secondary" align="center">
+    {'Copyright © '}
+    <Link color="inherit" href="https://github.com/RIFT-tokyo">
+      RIFT-tokyo
+    </Link>{' '}
+    {new Date().getFullYear()}.
+  </Typography>
+);
 
-const Footer: React.FC<Props> = ({ children }) => (
-  <Box sx={{ pb: 10 }}>
-    <Paper
-      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, pb: 2 }}
-      elevation={3}
-    >
-      <BottomNavigation>
-        <Grid container justifyContent="center">
-          {children}
-        </Grid>
-      </BottomNavigation>
-    </Paper>
+const Footer: React.VFC = () => (
+  <Box
+    component="footer"
+    sx={{
+      bgcolor: 'background.paper',
+      width: '100%',
+      position: 'static',
+      bottom: 0,
+      pb: 10,
+    }}
+  >
+    <Container>
+      <Typography
+        variant="h6"
+        align="center"
+        gutterBottom
+        sx={{
+          fontFamily: 'Zen Tokyo Zoo',
+        }}
+      >
+        TRANSCENDENCE
+      </Typography>
+      <Typography
+        variant="subtitle2"
+        align="center"
+        color="text.secondary"
+        component="p"
+      >
+        {'uses '}
+        <Link color="inherit" href="https://icons8.com">
+          icons8.com
+        </Link>{' '}
+      </Typography>
+      <Copyright />
+    </Container>
   </Box>
 );
 
