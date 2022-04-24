34818d0ddd285ac7ce02eac675224e0a4444977a
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-04-24 15:49:19 +0900

    add achievement detail modal

--- src/components/model/AchievementList.tsx
+++ src/components/model/AchievementList.tsx
@@ -1,4 +1,5 @@
-import { Stack, Typography } from '@mui/material';
+import { Modal, Stack, Typography, Box } from '@mui/material';
+import { useState } from 'react';
 import { Achievement } from '../../api/generated';
 
 interface AchievementBadgeProps {
@@ -9,11 +10,51 @@ interface AchievementListProps {
   achievements: Achievement[];
 }
 
-const AchievementBadge = ({ achievement }: AchievementBadgeProps) => (
-  <Stack paddingRight={0.5}>
-    <img width={60} height={60} src={achievement.image} alt={achievement.name} />
-  </Stack>
-);
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+const AchievementBadge = ({ achievement }: AchievementBadgeProps) => {
+  const [isOpenModal, toggleModal] = useState(false);
+  return (
+    <Stack paddingRight={0.5}>
+      <Stack onClick={() => toggleModal(true)}>
+        <img
+          width={60}
+          height={60}
+          src={achievement.image}
+          alt={achievement.name}
+        />
+      </Stack>
+      <Modal
+        open={isOpenModal}
+        onClose={() => toggleModal(false)}
+      >
+        <Box sx={style}>
+          <Stack direction="row" alignItems="center">
+            <img
+              width={100}
+              height={100}
+              src={achievement.image}
+              alt={achievement.name}
+            />
+            <Stack>
+              <Typography variant="h3">{achievement.name}</Typography>
+              <Typography variant="body1">{achievement.description}</Typography>
+            </Stack>
+          </Stack>
+        </Box>
+      </Modal>
+    </Stack>
+  );
+}
 
 const AchievementList = ({ achievements }: AchievementListProps) => (
   <Stack>
