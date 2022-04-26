f5d53ef77c925a7670d58413c9b105e5fdc58bae
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-04-26 21:46:18 +0900

    fix accout setting button

--- src/components/pages/Settings.tsx
+++ src/components/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress, Grid, Stack } from '@mui/material';
+import { CircularProgress, Grid, Stack } from '@mui/material';
 import * as React from 'react';
 import { useSnackbar } from 'notistack';
 import { User, UserApi } from '../../api/generated/api';
@@ -6,7 +6,6 @@ import { AuthContext } from '../../contexts/AuthContext';
 import AccountSetting from '../model/AccountSetting';
 import SecuritySetting from '../model/SecuritySetting';
 import ErrorRouter from '../ui/ErrorRouter';
-import Footer from '../ui/Footer';
 import SettingTab from '../ui/SettingTab';
 
 type Props = {
@@ -52,6 +51,7 @@ const Settings: React.VFC<Props> = ({ active }: Props) => {
       const { data } = await userApi.putUsersUserId(user.id!, requestBody, {
         withCredentials: true,
       });
+      enqueueSnackbar('Profile updated', { variant: 'success' });
       setUser(data);
     } catch (err: any) {
       enqueueSnackbar(err.response.data.message, { variant: 'error' });
@@ -61,38 +61,10 @@ const Settings: React.VFC<Props> = ({ active }: Props) => {
 
   let settingContent = <CircularProgress />;
   if (active === 'Account' && !loading && user) {
-    settingContent = <AccountSetting user={user} setUser={setUser} />;
+    settingContent = <AccountSetting user={user} setUser={setUser} submit={submit} reset={reset}/>;
   } else if (active === 'Security' && !loading) {
     settingContent = <SecuritySetting />;
   }
-
-  const getFooter = (activeSetting: string) => {
-    if (activeSetting === 'Account') {
-      return (
-        <Footer>
-          <Stack direction="row" margin={2} spacing={2}>
-            <Button
-              variant="contained"
-              color="inherit"
-              size="large"
-              onClick={reset}
-            >
-              reset
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              size="large"
-              onClick={submit}
-            >
-              save
-            </Button>
-          </Stack>
-        </Footer>
-      );
-    }
-    return null;
-  };
 
   return (
     <ErrorRouter statusCode={statusCode}>
@@ -102,7 +74,6 @@ const Settings: React.VFC<Props> = ({ active }: Props) => {
           {settingContent}
         </Stack>
       </Grid>
-      {getFooter(active)}
     </ErrorRouter>
   );
 };
