efd9c35a6f6e382d9b1c74fedc0dc7a64fa03aca
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-04-27 21:21:45 +0900

    add game canvas

--- src/components/pages/Pong.tsx
+++ src/components/pages/Pong.tsx
@@ -1,5 +1,8 @@
-import { Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
+import { Sphere } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import { useContext, useEffect } from 'react';
+import { blueGrey } from '@mui/material/colors';
 import { AuthContext } from '../../contexts/AuthContext';
 import { SocketContext } from '../../contexts/SocketContext';
 import { EVENT } from '../config/constants';
@@ -23,9 +26,42 @@ const Pong = () => {
           userID: authUser?.id,
         });
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [client]);
 
-  return <Typography variant="h2">This page is for Pong</Typography>;
+  return (
+    <Container component="main" maxWidth="xl" sx={{ padding: 2 }}>
+      <Box
+        component="div"
+        zIndex={500}
+        m={2}
+        p={2}
+        sx={{ position: 'absolute' }}
+        alignItems="center"
+        justifyItems="center"
+      >
+        <Typography
+          variant="h2"
+          color={blueGrey[100]}
+          sx={{
+            fontFamily: 'Zen Tokyo Zoo',
+          }}
+        >
+          PONG
+        </Typography>
+      </Box>
+      <Box component="div">
+        <Canvas
+          style={{ height: 'calc(100vh - 172px)', backgroundColor: blueGrey[800] }}
+        >
+          <ambientLight />
+          <Sphere>
+            <meshNormalMaterial />
+          </Sphere>
+        </Canvas>
+      </Box>
+    </Container>
+  );
 };
 
 export default Pong;
