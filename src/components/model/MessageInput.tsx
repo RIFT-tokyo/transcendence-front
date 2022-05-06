500e5b6e1be77daba81997c86fef9b35e8a6411a
nfukada <42476527+nafuka11@users.noreply.github.com>
2022-05-06 21:53:06 +0900

    fix(messageinput): fix import

--- src/components/model/MessageInput.tsx
+++ src/components/model/MessageInput.tsx
@@ -1,4 +1,5 @@
-import { TextField } from '@mui/material';
+import { Send } from '@mui/icons-material';
+import { IconButton, TextField } from '@mui/material';
 import { ChangeEvent, useState } from 'react';
 
 const MessageInput = () => {
