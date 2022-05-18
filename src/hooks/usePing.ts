47de2c85b272afe4472bb98ea6420bd0d2ba7efc
sasakiyudai <yudai14142@gmail.com>
2022-05-18 21:22:41 +0900

    memoize emitPing

--- src/hooks/usePing.ts
+++ src/hooks/usePing.ts
@@ -1,14 +1,14 @@
-import { useContext, useEffect } from 'react';
+import { useCallback, useContext } from 'react';
 import { EVENT } from '../components/config/constants';
 import { SocketContext } from '../contexts/SocketContext';
 
 const usePing = () => {
   const { client } = useContext(SocketContext);
-  const emitPing = () => {
+  const emitPing = useCallback(() => {
     if (client) {
       client.index.emit(EVENT.PING);
     }
-  };
+  }, [client]);
 
   return { emitPing };
 };
