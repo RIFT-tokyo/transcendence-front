bdd4a17966b6df51106149ad5b4e47c608dc2ef8
sasakiyudai <yudai14142@gmail.com>
2022-06-07 00:02:21 +0900

    onClick, emit SEND_MESSAGE

--- src/components/model/MessageInput.tsx
+++ src/components/model/MessageInput.tsx
@@ -1,12 +1,28 @@
 import { Send } from '@mui/icons-material';
 import { IconButton, TextField } from '@mui/material';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useContext, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+import { SocketContext } from '../../contexts/SocketContext';
+import { EVENT } from '../config/constants';
 
 const MessageInput = () => {
+  const { channelId } = useParams();
+  const { authUser } = useContext(AuthContext);
+  const { client } = useContext(SocketContext);
   const [text, setText] = useState('');
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
+  };
+
+  const sendMessage = async () => {
+    client.channels.emit(EVENT.SEND_MESSAGE, {
+      text,
+      userID: authUser?.id,
+      channelID: channelId,
+    });
+    setText('');
   };
 
   return (
@@ -22,7 +38,12 @@ const MessageInput = () => {
       required
       InputProps={{
         endAdornment: (
-          <IconButton aria-label="send message" edge="end">
+          <IconButton
+            onClick={() => sendMessage()}
+            aria-label="send message"
+            edge="end"
+            disabled={text.length === 0}
+          >
             <Send />
           </IconButton>
         ),
