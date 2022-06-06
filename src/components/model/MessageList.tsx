2013f27c8c059cf023fde606114f65f0c5aed014
sasakiyudai <yudai14142@gmail.com>
2022-06-07 00:01:45 +0900

    subscribe JOIN_CHANNEL_FROM_SERVER & SEND_MESSAGE_FROM_SERVER, emit JOIN_CHANNEL & LEAVE_CHANNEL. + auto scroll

--- src/components/model/MessageList.tsx
+++ src/components/model/MessageList.tsx
@@ -2,10 +2,18 @@ import { Stack, Typography } from '@mui/material';
 import TagIcon from '@mui/icons-material/Tag';
 import LockIcon from '@mui/icons-material/Lock';
 import { useOutletContext } from 'react-router-dom';
+import {
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import { Channel } from '../../api/generated';
 import MessageContent from './MessageContent';
 import MessageInput from './MessageInput';
-import { CHAT_MESSAGE_CONTENT_HEIGHT } from '../config/constants';
+import { CHAT_MESSAGE_CONTENT_HEIGHT, EVENT } from '../config/constants';
+import { SocketContext } from '../../contexts/SocketContext';
 
 type Context = {
   channel: Channel | null;
@@ -20,26 +28,36 @@ const channelIcon = (isProtected: boolean) => {
 
 const MessageList = () => {
   const { channel } = useOutletContext<Context>();
+  const { client } = useContext(SocketContext);
+  const [messages, setMessages] = useState<any[]>([]);
+  const scrollBottomRef = useRef<HTMLDivElement>(null);
 
-  const fetchMessages = (selectedChannel: Channel) =>
-    selectedChannel?.id
-      ? [...Array(selectedChannel.id)].map((_, index) => (
-          <MessageContent
-            // eslint-disable-next-line react/no-array-index-key
-            key={index}
-            user={
-              index % 2
-                ? { id: 1, username: 'testUser' }
-                : { id: 2, username: 'dummyUser' }
-            }
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            createdAt={new Date()}
-          />
-        ))
-      : null;
+  useEffect(() => {
+    client.channels.on(EVENT.JOIN_CHANNEL_FROM_SERVER, (data: any) => {
+      setMessages(data.messages);
+    });
+    client.channels.on(EVENT.SEND_MESSAGE_FROM_SERVER, (data: any) => {
+      setMessages((prev) => [...prev, data]);
+    });
+    client.channels.emit(EVENT.JOIN_CHANNEL, {
+      channelID: channel?.id,
+    });
+    return () => {
+      client.channels.off(EVENT.JOIN_CHANNEL_FROM_SERVER);
+      client.channels.off(EVENT.SEND_MESSAGE_FROM_SERVER);
+      client.channels.emit(EVENT.LEAVE_CHANNEL, {
+        channelID: channel?.id,
+      });
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [channel]);
+
+  useLayoutEffect(() => {
+    scrollBottomRef?.current?.scrollIntoView();
+  }, [messages]);
 
   return (
-    <Stack>
+    <Stack width={880}>
       <Stack
         direction="row"
         alignItems="center"
@@ -56,7 +74,16 @@ const MessageList = () => {
         height={CHAT_MESSAGE_CONTENT_HEIGHT}
         sx={{ overflowY: 'auto' }}
       >
-        {channel && fetchMessages(channel)}
+        {channel &&
+          messages.map((message) => (
+            <MessageContent
+              key={message.id}
+              user={message.user}
+              text={message.text}
+              createdAt={message.createdAt}
+            />
+          ))}
+        <div ref={scrollBottomRef} />
       </Stack>
       <MessageInput />
     </Stack>
